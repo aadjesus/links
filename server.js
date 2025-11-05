2a70304d6f66066590daaefaf4c0c8f045050e60
aadjesus <aadjesus@outlook.com>
2025-11-05 14:14:30 -0300

    criando

---
+++ server.js
@@ -0,0 +1,22 @@
+﻿// server.js
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const port = 3000;
+
+// Ajuste para ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Servir arquivos estáticos
+app.use(express.static(__dirname));
+
+app.get("/", (_, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(port, () => {
+    console.log(`🔥 LinkList Dark rodando em http://localhost:${port}`);
+});
